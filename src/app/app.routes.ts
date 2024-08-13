1ff7a4e8025882465f725d941e3cd2309cf423d7
Lucas Goulart <lucas.goulart@wearedev.com.br>
2024-08-12 22:22:07 -0300

    :tada: Inicialização projeto DIO - BLOG. O seu blog pessoal!!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FeedComponent } from './pages/feed/feed.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "/home", component: HomeComponent},
+    {path: "/feed", component: FeedComponent},
+    {path: "/contact", component: ContactComponent}
+];
